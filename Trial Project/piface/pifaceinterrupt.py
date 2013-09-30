e14e9a1f733c59af57655b248a32ebe260997336
mrtunadeluxe <jamesryan3@cmail.carleton.ca>
2013-09-30 16:39:24 -0400

    Update pifaceinterrupt.py
    minor code changes

--- Trial Project/piface/pifaceinterrupt.py
+++ Trial Project/piface/pifaceinterrupt.py
@@ -2,15 +2,16 @@
 
 import pifacecommon
 import time
+import os
 
-count = 0
+quit = False
 
 def pressed_button(event):
-    print('*', end='')
-	
 	#Enter UDP packet stuff here.
-	
-	count =+ 1
+
+def pressed_quit(event):
+    global quit
+    quit = True
 
 pifacecommon.core.init()
 
@@ -21,12 +22,13 @@ listener = pifacecommon.interrupts.PortEventListener(port)
 
 # set up listeners for buttons
 listener.register(0, pifacecommon.interrupts.IODIR_ON, pressed_button)
+listener.register(3, pifacecommon.interrupts.IODIR_ON, stop_listening)
 
 # Start listening for button presses. spawns a new thread.
 listener.activate()
 
 # hang around until counter hits 20.
-while count < 20:
+while not quit:
     time.sleep(1)
 
 #closes the listener
